b935e900e6635b623ccd9101c38eb743ee763452
hammad93 <ham.u@live.com>
2020-09-18 23:55:09 -0400

    Change script so that it can be imported as a class

--- update.py
+++ update.py
@@ -19,25 +19,6 @@ from pytz import timezone
 import zipfile
 import io
 
-# this link can be reused to download the most recent data
-static_link = 'https://www.nhc.noaa.gov/gis/kml/nhc_active.kml'
-# common timezones for parsing with dateutil. offset by seconds
-timezones = {
-    "ADT": 4 * 3600,
-    "AST": 3 * 3600,
-    "CDT": -5 * 3600,
-    "CST": -6 * 3600,
-    "CT": -6 * 3600,
-    "EDT": -4 * 3600,
-    "EST": -5 * 3600,
-    "ET": -5 * 3600,
-    "GMT": 0 * 3600,
-    "PST": -8 * 3600,
-    "PT": -8 * 3600,
-    "UTC": 0 * 3600,
-    "Z": 0 * 3600,
-}
-
 def past_track(link):
     '''
     From a KMZ file of a storm in the NHC format, we extract the history
@@ -70,13 +51,7 @@ def past_track(link):
                 time = datetime.strptime(entry['atcfdtg'],
                                         '%Y%m%d%H').replace(
                     tzinfo=timezone('UTC'))
-                '''
-                print(f'Time: {time}, '
-                      f'Wind: {entry["intensity"]}, '
-                      f'Lat: {entry["lat"]}, '
-                      f'Lon: {entry["lon"]}, '
-                      f'Pressure: {entry["minSeaLevelPres"]}')
-                '''
+
                 # add to results
                 kml['results'].append({
                     'time' : time,
@@ -89,45 +64,84 @@ def past_track(link):
 
     return kml
 
-if __name__ == "__main__" :
+def nhc() :
+    '''
+    Runs the NHC update and populates current Atlantic storms
+
+    Returns
+    -------
+    array of dict
+        Each dictionary is in the following form,
+        {
+            "storm" : string # the storm ID from the NHC
+            "metadata" : dict # the kml files used to create the results
+            "entries" : array of dict # The data for the storm in the form,
+                {
+                    'time' : Datetime,
+                    'wind' : Knots,
+                    'lat' : Decimal Degrees,
+                    'lon' : Decimal Degrees,
+                    'pressure' : Barometric pressure (mb)
+                }
+        }
+    '''
+    # this link can be reused to download the most recent data
+    static_link = 'https://www.nhc.noaa.gov/gis/kml/nhc_active.kml'
+    # common timezones for parsing with dateutil. offset by seconds
+    timezones = {
+        "ADT": 4 * 3600,
+        "AST": 3 * 3600,
+        "CDT": -5 * 3600,
+        "CST": -6 * 3600,
+        "CT": -6 * 3600,
+        "EDT": -4 * 3600,
+        "EST": -5 * 3600,
+        "ET": -5 * 3600,
+        "GMT": 0 * 3600,
+        "PST": -8 * 3600,
+        "PT": -8 * 3600,
+        "UTC": 0 * 3600,
+        "Z": 0 * 3600,
+    }
+
     # create data structure as dictionary
     request = requests.get(static_link)
     data = xmltodict.parse(request.text)
     results = []
 
     # parse in storms
-    for folder in data['kml']['Document']['Folder'] :
+    for folder in data['kml']['Document']['Folder']:
         # the id's that start with 'at' are the storms we are interested in
         # others can include 'wsp' for wind speed probabilities
-        if folder['@id'][:2] == 'at' :
+        if folder['@id'][:2] == 'at':
             # some storms don't have any data because they are so weak
-            if not 'ExtendedData' in folder.keys() :
+            if not 'ExtendedData' in folder.keys():
                 continue
 
             # storm data structure
             storm = {
-                'metadata' : folder,
-                'entries' : []
+                'metadata': folder,
+                'entries': []
             }
             entry = {}
 
-            for attribute in folder['ExtendedData'][1] :
-                if attribute == 'tc:atcfID' : # NHC Storm ID
+            for attribute in folder['ExtendedData'][1]:
+                if attribute == 'tc:atcfID':  # NHC Storm ID
                     storm['id'] = folder['ExtendedData'][1][attribute]
-                elif attribute == 'tc:name' : # Human readable name
+                elif attribute == 'tc:name':  # Human readable name
                     print(folder['ExtendedData'][1][attribute])
-                elif attribute == 'tc:centerLat' : # Latitude
+                elif attribute == 'tc:centerLat':  # Latitude
                     entry['lat'] = float(folder['ExtendedData'][1][attribute])
-                elif attribute == 'tc:centerLon' : # Longitude
+                elif attribute == 'tc:centerLon':  # Longitude
                     entry['lon'] = float(folder['ExtendedData'][1][attribute])
-                elif attribute == 'tc:dateTime' : # Timestamp
+                elif attribute == 'tc:dateTime':  # Timestamp
                     entry['time'] = dateutil.parser.parse(
                         folder['ExtendedData'][1][attribute],
-                        tzinfos = timezones)
-                elif attribute == 'tc:minimumPressure' : # Barometric pressure
+                        tzinfos=timezones)
+                elif attribute == 'tc:minimumPressure':  # Barometric pressure
                     entry['pressure'] = float(folder['ExtendedData'][1]
                                               [attribute].split(' ')[0])
-                elif attribute == 'tc:maxSustainedWind' : # Wind Intensity
+                elif attribute == 'tc:maxSustainedWind':  # Wind Intensity
                     # note that we are converting mph to knots
                     entry['wind'] = float(folder['ExtendedData'][1][attribute].
                                           split(' ')[0]) / 1.151
@@ -138,8 +152,8 @@ if __name__ == "__main__" :
             # add entry to storm
             storm['entries'].append(entry)
             # get network link and extract past history
-            for links in folder['NetworkLink'] :
-                if links['@id'] == 'pasttrack' :
+            for links in folder['NetworkLink']:
+                if links['@id'] == 'pasttrack':
                     kml = past_track(links['Link']['href'])
                     # add history to entries
                     storm['entries'].extend(kml['results'])
@@ -149,3 +163,7 @@ if __name__ == "__main__" :
 
             # add to results
             results.append(storm)
+
+
+if __name__ == "__main__" :
+    nhc()

05b2744d1505a05a3fbaae82ead408698f94c7a4
hammad93 <ham.u@live.com>
2021-08-22 09:09:03 -0400

    Debug

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,13 +6,13 @@ echo "Docker container has been started"
 declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
 
 # run the tensorflow server
-python /home/bitnami/hurricane-deploy/download_models.py
+python /hurricane-deploy/download_models.py
 nohup tensorflow_model_server --model_base_path=/root/forecast --rest_api_port=9000 --model_name=hurricane 2>&1 &
 
 # Setup a cron schedule
 echo "SHELL=/bin/bash
 BASH_ENV=/container.env
-0 * * * * python /home/bitnami/hurricane-deploy/report.py >> /var/log/cron.log 2>&1
+0 * * * * python /hurricane-deploy/report.py >> /var/log/cron.log 2>&1
 # This extra line makes it a valid cron" > scheduler.txt
 
 crontab scheduler.txt

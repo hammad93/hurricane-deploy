065fe059941c309a06b04ebceebf19844a2b249c
hammad93 <ham.u@live.com>
2021-08-22 08:56:16 -0400

    Debug

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,7 +12,7 @@ nohup tensorflow_model_server --model_base_path=/root/forecast --rest_api_port=9
 # Setup a cron schedule
 echo "SHELL=/bin/bash
 BASH_ENV=/container.env
-0 * * * * python /hurricane-deploy/report.py >> /var/log/cron.log 2>&1
+0 * * * * python /home/bitnami/hurricane-deploy/report.py >> /var/log/cron.log 2>&1
 # This extra line makes it a valid cron" > scheduler.txt
 
 crontab scheduler.txt

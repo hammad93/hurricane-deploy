6678ac298217afd5762dae20ceabe9420b504c12
Hammad Usmani <ham.u@live.com>
2021-09-23 18:15:02 -0400

    add time to report

--- report.py
+++ report.py
@@ -75,6 +75,7 @@ for storm in data :
 
     # storm metadata
     html += f"""<h3>
+    As of {str(storm['entries'][-1]['time'])}
     Wind : {round(1.150779 * storm['entries'][-1]['wind'])} mph, {storm['entries'][-1]['wind']} Knots<br>
     Pressure : {storm['entries'][-1]['pressure']} mb<br>
     Location : (lat, lon) ({storm['entries'][-1]['lat']}, {storm['entries'][-1]['lon']}<br>)

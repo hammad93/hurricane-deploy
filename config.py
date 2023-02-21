5c5c805c7a2c53185e1598b5c3cd790262a8f1d7
Hammad Usmani <ham.u@live.com>
2023-02-20 20:35:18 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 feature_scaler_path='https://github.com/apatel726/HurricaneDissertation/raw/master/hurricane_ai/scaler/feature_scaler.pkl'
-email_model='https://storage.googleapis.com/cyclone-ai.appspot.com/model_2021_09_23_03_27/saved_model.pb'
+forecast_model='https://storage.googleapis.com/cyclone-ai.appspot.com/model_2021_09_23_03_27/saved_model.pb'
+forecast_model_dir='/root/forecast/1'

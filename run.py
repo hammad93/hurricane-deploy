547d29e72776f9eeabdd3fcb315880c35a00472b
Hammad Usmani <ham.u@live.com>
2023-05-07 23:35:30 -0400

    Update run.py

--- run.py
+++ run.py
@@ -9,6 +9,18 @@ import pandas as pd
 import os
 
 app = FastAPI()
+origins = [
+    "http://localhost:3000",
+    # Add more origins if required
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 cache = {}
 
 @app.get("/")

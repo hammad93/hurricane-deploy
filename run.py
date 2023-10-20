d0e5a23c5f1b2f5da7ec5e8e9e567533888176dc
Hammad Usmani <ham.u@live.com>
2023-10-20 17:46:47 -0400

    Update run.py with updated openai credentials

--- run.py
+++ run.py
@@ -125,4 +125,5 @@ if __name__ == "__main__":
     # Set ChatGPT password
     passwords = pd.read_csv(config.credentials_dir)
     os.environ["OPENAI_API_KEY"] = passwords[passwords['user'] == 'openai'].iloc[0]['pass']
+    os.environ["OPENAI_API_BASE"] = passwords[passwords['user'] == 'openai'].iloc[0]['host']
     uvicorn.run("run:app", host="0.0.0.0", port=1337, reload=True)
